a9a0e6b88b72454956d07f207002d1f700b2e0f5
Nick Digiovanni (RIT Student) <nsd3407@rit.edu>
2025-04-03 18:22:13 -0400

    UI Proving to be tricky, will need a refactor

--- CanvasObject.h
+++ CanvasObject.h
@@ -3,15 +3,37 @@
 #ifndef _CanvasObject_
 #define _CanvasObject_
 
-
 #include "UI.h"
+#include "Canvas.h"
 
-using namespace DirectX;
-
-namespace UI
+class CanvasObject /*: public Canvas*/  // Inherit from Canvas to allow for rendering in the canvas
 {
-	
-}
+public:
+	// Constructor and Destructor
+	CanvasObject();
+	~CanvasObject();
+
+	// Public Variables
+	AnchorPoint anchorPoint; // The anchor point of the object
+	XMFLOAT2 position; // The position of the object in 2D space
+
+	// Public Methods
+	/*void Render(Microsoft::WRL::ComPtr<ID3D11DeviceContext> context) override;
+	virtual void OnResize(float width, float height) override;*/
+protected:
+	// Protected Variables
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer> indexBuffer;
+	unsigned int indexCount;
+
+	// protected Methods
+	virtual void Draw() = 0;
+private:
+	// Private Variables
+
+	// Private Methods
+	void SendToGPU(Microsoft::WRL::ComPtr<ID3D11DeviceContext> context);
+};
 
 
 #endif // !_CanvasObject_
